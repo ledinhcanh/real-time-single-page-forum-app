09aeee53d2074a621fc89dfb094c398576b1f29d
ledinhcanh <lecanh2810@gmail.com>
2019-12-06 11:35:32 +0700

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    // protected $fillable = ['title','slug','body','category_id','user_id'];
+    protected $guarded = [];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
+    }
 }
